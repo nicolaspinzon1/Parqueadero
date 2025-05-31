df69ddd16c6d21f087ba3496c4fbb0e1d9b11625
Nicolas pinzon <nicolasnnnnn7@gmail.com>
2025-05-31 18:07:08 -0500

    segundo avance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
+  vite: {
     plugins: [tailwindcss()],
+    server: {
+      proxy: {
+        // Redirige todas las peticiones a /api al backend Flask
+        '/api': 'http://127.0.0.1:5000',
+      },
+    },
   },
 });
-
